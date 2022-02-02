817550487a9877586c7f78c6661e8b9e3cdf2de1
Felipe <dev03.nuvem3@outlook.com>
2022-02-02 15:17:18 -0300

    Commit inicial

---
+++ ConnectToFireBirdDB/FireBird.cs
@@ -0,0 +1,42 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Data;
+
+namespace ConnectToFireBirdDB
+{
+    public class FireBird
+    {        
+        private readonly FbConnection Fbcon;
+
+        public FireBird(string connectionString)
+        {
+            Fbcon = new(connectionString);
+        }        
+
+        public DataTable DataTableRetornar(string sql)
+        {            
+            DataTable dataTable = new();
+
+            try
+            {
+                Fbcon.Open();
+
+                FbCommand fbCmd = new(sql, Fbcon);
+
+                FbDataAdapter fbDa = new(fbCmd);
+
+                fbDa.Fill(dataTable);
+            }
+            catch (FbException fbex)
+            {
+                Console.WriteLine("Erro ao acessar o FireBird " + fbex.Message, "Erro");
+            }
+            finally
+            {
+                Fbcon.Close();
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ ConnectToFireBirdDB/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using ClosedXML.Excel;
+
+namespace ConnectToFireBirdDB
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            GenerateExcelFireBird();
+        }
+
+        private static void GenerateExcelFireBird()
+        {
+            string connectionString = Configuration.GetConnectionString("FireBirdConnection").ToString();
+
+            FireBird fb = new(connectionString);            
+
+            DataTable dataTable = fb.DataTableRetornar("SELECT * FROM PRODUTO");
+
+            XLWorkbook wb = new();            
+
+            wb.Worksheets.Add(dataTable, "Produtos");
+
+            wb.SaveAs("ProdutosImportados.xlsx");
+        }
+    }
+}
